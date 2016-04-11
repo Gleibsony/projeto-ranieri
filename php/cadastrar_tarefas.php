9f45816a231c0de8ed7e8eb9f7790341cebae7a0
gleibsony <gleibsonytalo12@gmail.com>
2016-04-11 11:41:19 -0300

    cadastro e home

---
+++ php/cadastrar_tarefas.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Cadastrar Tarefas</title>
+    </head>
+    <body>
+        <div>
+            </form>
+                <fieldset>
+                    <label>Titulo</label>
+                    <input type="text" name="titulo" required>
+                    <label>Descrição</label>
+                    <textarea name="descricao" cols="25" rows="15"></textarea>
+                    <label>Data</label>
+                    <input type="date" name="data" required>
+                </fieldset>
+            </form>
+
+        </div>
+    </body>
+</html>
+
+            
---
+++ php/home.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <fieldset>
+        <button type="submit">Cadastrar Tarefas</button>
+        <button type="submit">Remover Tarefas</button>
+        <button type="submit">Editar Tarefas</button>
+    </fieldset>
+    </body>
+</html>
